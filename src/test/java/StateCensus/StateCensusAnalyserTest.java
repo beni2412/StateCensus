2748d18afbac6cccd64f9299e49776e7808ab1f3
beni2412 <hardaman97@gmail.com>
2020-10-23 22:16:12 +0530

    [Hardaman] Added: Test case 2 to check if CSV file is correct

--- src/test/java/StateCensus/StateCensusAnalyserTest.java
+++ src/test/java/StateCensus/StateCensusAnalyserTest.java
@@ -1,15 +1,32 @@
 package StateCensus;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StateCensusAnalyserTest {
+	private StateCensusAnalyser stateCensusAnalyser;
 	public static final String STATE_CENSUS_FILE_PATH = "./IndiaStateCensusData.csv";
-	
+	private static final String STATE_CENSUS_WRONG_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+
+	@Before
+	public void initialize() {
+		stateCensusAnalyser = new StateCensusAnalyser();
+
+	}
+
 	@Test
-	public void givenStateCensusCSVFile_ShouldReturnNumberOfRecords() throws CensusAnalyserException{
-		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+	public void givenStateCensusCSVFile_ShouldReturnNumberOfRecords() throws CensusAnalyserException {
 		int numOfEntries = stateCensusAnalyser.loadIndiaCensusData(STATE_CENSUS_FILE_PATH);
 		Assert.assertEquals(29, numOfEntries);
+	}
+
+	@Test
+	public void givenStateCensus_WrongCSVFile_ShouldThrowException() {
+		try {
+			stateCensusAnalyser.loadIndiaCensusData(STATE_CENSUS_WRONG_FILE_PATH);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+		}
 	}
 }
